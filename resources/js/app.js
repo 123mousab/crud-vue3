b432aa1659d3a20e4be5161a68ae089c255db02e
Mousab Salah <mousabsalaham@gmail.com>
2022-07-26 10:12:05 +0300

    add-pagiantion

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,11 @@ import '../css/app.css';
 
 // For Vue.js
 import { createApp } from 'vue/dist/vue.esm-bundler'
-
 import PostIndex from './components/Posts/index.vue'
+import LaravelVuePagination from 'laravel-vue-pagination';
 
 const app = createApp({})
 app.component('posts-index', PostIndex)
+app.component('Pagination', LaravelVuePagination)
 
 app.mount('#app')
--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -3,9 +3,9 @@ import { ref } from 'vue';
 export default function usePosts() {
     const posts = ref([]);
 
-    const getPosts = async () => {
-        axios.get('/api/posts')
-            .then(res => posts.value = res.data.data)
+    const getPosts = async (page = 1) => {
+        axios.get('/api/posts?page=' +page)
+            .then(res => posts.value = res.data)
             .catch(error => console.log(error))
     }
 
