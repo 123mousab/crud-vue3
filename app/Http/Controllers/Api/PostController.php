50d4f0058625c44903150df59567254a9d140f88
Mousab Salah <mousabsalaham@gmail.com>
2022-07-27 10:47:00 +0300

    up

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -12,20 +12,12 @@ class PostController extends Controller
 {
     public function index()
     {
-        $orderColumn = request('order_column', 'created_at');
-        if (!in_array($orderColumn, ['id', 'title', 'created_at'])) {
-            $orderColumn = 'created_at';
-        }
-        $orderDirection = request('order_direction', 'desc');
-        if (!in_array($orderDirection, ['asc', 'desc'])) {
-            $orderDirection = 'desc';
-        }
         $posts = Post::with('category')
-            ->when(request('category'), function ($query) {
-                $query->where('category_id', request('category'));
-            })
-            ->orderBy($orderColumn, $orderDirection)
+            ->filter()
+            ->search()
+            ->order()
             ->paginate(50);
+
         return PostResource::collection($posts);
     }
 
--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -9,14 +9,34 @@ export default function usePosts() {
     const post = ref({})
     const swal = inject('$swal')
 
+    /**
+     *
+     * @param page
+     * @param search_category
+     * @param search_id
+     * @param search_title
+     * @param search_content
+     * @param search_global
+     * @param order_column
+     * @param order_direction
+     * @returns {Promise<void>}
+     */
     const getPosts = async (
         page = 1,
-        category = '',
+        search_category = '',
+        search_id = '',
+        search_title = '',
+        search_content = '',
+        search_global = '',
         order_column = 'created_at',
         order_direction = 'desc'
     ) => {
         axios.get('/api/posts?page=' + page +
-            '&category=' + category +
+            '&search_category=' + search_category +
+            '&search_id=' + search_id +
+            '&search_title=' + search_title +
+            '&search_content=' + search_content +
+            '&search_global=' + search_global +
             '&order_column=' + order_column +
             '&order_direction=' + order_direction)
             .then(response => {
@@ -24,6 +44,11 @@ export default function usePosts() {
             })
     }
 
+    /**
+     *
+     * @param post
+     * @returns {Promise<void>}
+     */
     const storePosts = async (post) => {
         if (isLoading.value) return;
 
@@ -54,6 +79,11 @@ export default function usePosts() {
             .finally(() => isLoading.value = false)
     }
 
+    /**
+     *
+     * @param id
+     * @returns {Promise<void>}
+     */
     const getPost = async (id) => {
         axios.get('/api/posts/' + id)
             .then(response => {
@@ -61,6 +91,11 @@ export default function usePosts() {
             })
     }
 
+    /**
+     *
+     * @param post
+     * @returns {Promise<void>}
+     */
     const updatePost = async (post) => {
         if (isLoading.value) return;
 
@@ -83,6 +118,11 @@ export default function usePosts() {
             .finally(() => isLoading.value = false)
     }
 
+    /**
+     *
+     * @param id
+     * @returns {Promise<void>}
+     */
     const deletePost = async (id) => {
         swal({
             title: 'Are you sure?',
@@ -116,6 +156,7 @@ export default function usePosts() {
             })
 
     }
+
 
 
     return {
