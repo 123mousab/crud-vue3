f40d656d5691b042e3f23fa3365492cda778e19d
Mousab Salah <mousabsalaham@gmail.com>
2022-09-28 11:11:17 +0300

    structure design driven development

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::view('/{any?}', 'dashboard')
-    ->name('dashboard')
-    ->where('any', '.*');
+use Illuminate\Support\Facades\DB;
